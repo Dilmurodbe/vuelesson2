48dcfb3a003e18a5cc55ee222e959e054b607eca
dilmurodbe <saydullayevdilmurod233@gmail.com>
2023-01-24 15:49:56 +0500

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,97 @@
+<script>
+import Navbar from "./navbar.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  components: {
+    Navbar,
+  },
+};
+</script>
+<template>
+  <div class="home">
+    <Navbar />
+    <img class="home-img" src="../assets/img/home.jpg" alt="" />
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="text">
+        <h1>
+          Подбор <br />
+          недвижимости <br />
+          с полным сопровождением <br />
+          на Вашем языке
+        </h1>
+        <div class="text-button">
+          <p>
+            Наше агентство специализируется на премиум объектах и презентует их
+            Вам на русском языке. Подбор недвижимости во всей Франции с фокусом
+            на самых привилегированных и живописных районах Французского
+            Средиземноморья - Ницца, Канны или Лазурный Берег без лишних
+            временных затрат и наценнок.
+          </p>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between mt-5">
+        <p class="p15">Скачать презентацию</p>
+
+        <button @click="count++" class="header-button">
+          count:{{ count }}
+          <p>Напишите нам</p>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.home {
+  display: flex;
+  position: relative;
+}
+.home-img {
+  width: 100%;
+
+  object-fit: cover;
+  position: absolute;
+  z-index: -1;
+}
+.text {
+  width: 65%;
+  padding-top: 200px;
+}
+
+.text h1 {
+  font-family: "Playfair Display";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 71px;
+  line-height: 90%;
+  color: #fff;
+}
+.text-button {
+  width: 53%;
+  color: #fff;
+}
+.p15 {
+  font-size: 15px;
+  color: #fff;
+  text-decoration: underline;
+}
+.header-button {
+  background: #fff9ec;
+  border-radius: 7px;
+  border: none;
+}
+.header-button p {
+  font-size: 15px;
+  color: #be7e00;
+  padding: 15px;
+  margin-bottom: 0rem;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+  name: 'NavbarPage'
+}
+</script>
+<template>
+  <div class="container">
+    <div class="row">
+<div class="d-flex justify-content-between">
+    
+<div class="col-auto mt-5 ">
+  <div class="navbar-left">
+  <div class="d-flex justify-content-around">
+  <img class="logo" src="../assets/img/Group 88.png" alt="">
+<a class="navbar-h1" href="">Azur Life</a>
+<img src="../assets/img/Vector 44.png" alt="">
+<a class="navbar-h1" href="" ><img src="../assets/img/Exclude.png" alt="">  Франция, бульвар Леклерк, 46, 06310, Болье-сюр-Мер
+</a><img src="../assets/img/Vector 44.png" alt="">
+
+
+<a class="navbar-h1" href=""> <img src="../assets/img/Vector.png" alt="">    +33 4 93 16 06 31</a>
+</div>
+</div>
+</div>
+
+<div class="col-auto mt-5">
+  <a class="navbar-language-changer-en" href="">en</a>
+  <a class="navbar-language-changer" href="">fr</a>
+  <a class="navbar-language-changer" href="">ru</a>
+  <a class="navbar-language-changer" href="">it</a>
+</div>
+</div>
+  </div></div>
+</template>
+
+<style>
+.logo{
+  width: 25px;
+  height: 20px;
+ margin: 0 15px;
+}
+
+.navbar-h1{
+  font-size: 16px;
+  color: rgb(255, 255, 255);
+  margin: 0 15px;
+   text-decoration: none;
+}
+.navbar-language-changer-en{
+  font-size: 15px;
+  color: #fff;
+  text-decoration: underline;
+  margin: 0 10px;
+
+}
+
+.navbar-language-changer{
+  font-size: 15px;
+  color: #fff;
+  margin: 0 10px;
+  text-decoration: none;
+}
+
+</style>
